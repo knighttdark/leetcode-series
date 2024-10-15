876ffec3fd6d60d45de743887a290fa5a8c959d9
thanhtranndang <thanhtranndang@gmail.com>
2024-10-15 09:42:38 +0700

    hatake

--- 1295. Find Numbers with Even Number of Digits/1295_Find_Numbers_with_Even_Number_of_Digits.cpp
+++ 1295. Find Numbers with Even Number of Digits/1295_Find_Numbers_with_Even_Number_of_Digits.cpp
@@ -1,7 +1,43 @@
-class Solution
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class FindNumber
 {
 public:
-    int findNumbers(vector<int> &nums)
+    static int findNumbers(vector<int> &nums)
     {
+        int count = 0;
+
+        for (int num : nums)
+        {
+            int temp = num;
+            int digital = 0;
+
+            while (temp > 0)
+            {
+                temp /= 10;
+                digital++;
+            }
+
+            if (digital % 2 == 0)
+            {
+                count++;
+            }
+        }
+        return count;
     }
 };
+
+int main()
+{
+    FindNumber findnumber;
+
+    vector<int> nums1 = {12, 345, 2, 6, 7896};
+    cout << "Output for nums1: " << findnumber.findNumbers(nums1) << endl;
+
+    vector<int> nums2 = {555, 901, 482, 1771};
+    cout << "Output for nums2: " << findnumber.findNumbers(nums2) << endl;
+
+    return 0;
+}
