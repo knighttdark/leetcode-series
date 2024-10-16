17ad8ab31d70d57abb50e6bc2906768660f552ae
thanhtranndang <thanhtranndang@gmail.com>
2024-10-16 21:26:28 +0700

    hatake

--- 989. Add to Array-Form of Integer/989_Add_to_Array_Form_of_Integer.cpp
+++ 989. Add to Array-Form of Integer/989_Add_to_Array_Form_of_Integer.cpp
@@ -8,28 +8,25 @@ class Solution
 public:
     vector<int> addToArrayForm(vector<int> &num, int k)
     {
-        long long numInt = 0;
-        for (int n : num)
-        {
-            numInt = numInt * 10 + n;
-        }
-        numInt += k;
-
         vector<int> result;
+        int ns = num.size();
+        int carry = 0;
 
-        if (numInt == 0)
+        for (int i = ns - 1; i >= 0 || k > 0 || carry > 0; i--)
         {
-            result.push_back(0);
+            int digit = (i >= 0 ? num[i] : 0);
+
+            int sum = digit + (k % 10) + carry;
+
+            result.push_back(sum % 10);
+
+            carry = sum / 10;
+
+            k /= 10;
         }
-        else
-        {
-            while (numInt > 0)
-            {
-                result.push_back(numInt % 10);
-                numInt /= 10;
-            }
-            reverse(result.begin(), result.end());
-        }
+
+        reverse(result.begin(), result.end());
+
         return result;
     }
 };
@@ -37,8 +34,8 @@ public:
 int main()
 {
     Solution s1;
-    vector<int> num = {2, 1, 5};
-    int k = 806;
+    vector<int> num = {1, 2, 0, 0};
+    int k = 34;
 
     vector<int> result = s1.addToArrayForm(num, k);
 
@@ -46,6 +43,5 @@ int main()
     {
         cout << n;
     }
-
     return 0;
 }
