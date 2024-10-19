3d97b47a0816fd2d1afa0f619e183c3805e6d3a3
thanhtranndang <thanhtranndang@gmail.com>
2024-10-19 09:53:23 +0700

    hatake

---
+++ 1672. Richest Customer Wealth/1672_Richest_Customer_Wealth.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+class Solution
+{
+public:
+    int maximumWealth(vector<vector<int>> &accounts)
+    {
+        int numRow = accounts.size();
+        int numCol = accounts[0].size();
+        int max = 0;
+        for (int i = 0; i < numRow; i++)
+        {
+            int summCurrent = 0;
+            for (int j = 0; j < numCol; j++)
+            {
+                summCurrent += accounts[i][j];
+            }
+            if (summCurrent > max)
+            {
+                max = summCurrent;
+            }
+        }
+        return max;
+    }
+};
+int main()
+{
+    Solution s1;
+    vector<vector<int>> acccounts = {{1, 2, 3}, {3, 2, 1}};
+
+    int result = s1.maximumWealth(acccounts);
+    cout << result;
+    return 0;
+}
